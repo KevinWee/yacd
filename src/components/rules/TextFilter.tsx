bc7a7cdc581b6cc9c46fa660ff33a9a9893f42fc
kevinwee <kevinwee@163.com>
2020-07-21 16:17:10 +0800

    代码风格自动调整

--- src/components/rules/TextFilter.tsx
+++ src/components/rules/TextFilter.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useTextInut } from 'src/hooks/useTextInput';
 import { ruleFilterText } from 'src/store/rules';
-import { useTranslation } from 'react-i18next';
 
 import shared from '../shared.module.css';
 
 export function TextFilter() {
   const [onChange, text] = useTextInut(ruleFilterText);
-  let { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <input
       className={shared.input}
